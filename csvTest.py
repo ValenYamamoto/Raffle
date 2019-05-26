ab6b9f2f4bc7c65cf34016dc7d641dd8b4efe218
Valen Yamamoto <valenryamamoto@gmail.com>
2019-05-26 09:21:38 -0700

    Adjustable # of Winners for each prize

--- csvTest.py
+++ csvTest.py
@@ -1,40 +1,43 @@
 import csv as csv
 import random as random
 
-file = open("hypo.csv", "w+")
+file = open("test2.csv", "w+")
 file.close()
 
-file = open("hypo.csv", mode='a', newline='')
+file = open("test2.csv", mode='a', newline='')
 entries = {}
 numbers = {}
+prizeNumbers = []
 prizeWinners = []
 
 print("OPEN")
 
-for i in range(1, 101):
-    numbers[i] = 0
-
 def readFile():
-    f = open("test.csv", mode='r', newline="")
+    f = open("Raffle Data.csv", mode='r', newline="")
     
     for row in csv.reader(f):
-        #self.entries[row[0]] = row[1]
-        if(len(row) > 0):
-            entries[row[0]] = row[1:]
+        if(row[0] == "# of Prizes"):
+            prizeNumbers.append(row[1:])
+        
+        elif(row[0] != "Name"):
+            #self.entries[row[0]] = row[1]
+            if(len(row) > 0):
+                entries[row[0]] = row[1:]
+
 
 def getWinners():
     readFile()
 
-    for prize in range(len(entries["1"])-1):
+    for prize in range(len(prizeNumbers[0])):
         winners = 0
-
-        while winners < 10:
+        
+        while winners < int(prizeNumbers[0][prize]):
             weight = []
             for i in list(entries.values()):
                 weight.append(int(i[prize]))
             randomVal = random.choices(list(entries), weights=weight, k=1)
             if randomVal[0] not in prizeWinners:
-                prizeWinners.append(randomVal[0])
+                prizeWinners.append([randomVal[0], prize])
                 entries.pop(randomVal[0])
                 winners = winners + 1
 
@@ -46,7 +49,7 @@ getWinners()
 for key in prizeWinners:
     #print(str(key))
     writer = csv.writer(file)
-    writer.writerow((key, 0))
+    writer.writerow((key[0], key[1]))
 file.close()
 print("Done")
 
